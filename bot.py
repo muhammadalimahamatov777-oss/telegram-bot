5563b5ca088b11f1e9613ca1ec39a085121d4cb6
muhammadalimahamatov777-oss <muhammadalimahamatov777@gmail.com>
2026-05-21 11:43:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import telebot
 from groq import Groq
 
 client = Groq(api_key="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")
-bot = telebot.TeleBot("8964935625:AAHU8CHepEwH81iTZsL1qwGjqY8mNvfkuDEKEN
+bot = telebot.TeleBot("8964935625:AAHU8CHepEwH81iTZsL1qwGjqY8mNvfkuDE")
+
 def ai_reply(text):
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
