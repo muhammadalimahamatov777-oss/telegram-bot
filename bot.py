23daefd831e1ed696f38e4ab1741ca1f8673a142
muhammadalimahamatov777-oss <muhammadalimahamatov777@gmail.com>
2026-05-21 11:59:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from groq import Groq
 
 client = Groq(api_key="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")
-bot = telebot.TeleBot("8964935625:AAHU8CHepEwH81iTZsL1qwGjqY8mNvfkuDE")
+bot = telebot.TeleBot("8964935625:AAHU8CHepEwH81iTZsL1qwGjqY8mNvfkuDE)
 
 def ai_reply(text):
     response = client.chat.completions.create(
@@ -17,6 +17,6 @@ def handle_message(message):
 
 @bot.business_message_handler(func=lambda m: True, content_types=['text'])
 def handle_business(message):
-    bot.reply_to(message, ai_reply(message.text))
+    bot.send_message(message.chat.id, ai_reply(message.text))
 
 bot.polling(allowed_updates=["message", "business_message"])
